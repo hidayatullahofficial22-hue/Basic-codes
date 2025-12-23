0b65d92a774f1199192e551f6a8b0077df50d23b
Hidayat Ullah <hidayatullah.official22@gmail.com>
2025-12-23 09:54:42 +0500

    Add files via upload
    This a program for " Guessing game".

---
+++ project02.py
@@ -0,0 +1,41 @@
+choice = input("Do you want to play the game?(yes/no)")
+
+while(choice != "no"):
+
+    from random import randint
+
+    n = randint (1,100)
+    
+    max_guess = 6
+
+    print("Note: You only have 6 attempts.")
+
+    user = int(input("Guess a Number in range of (1....100) please!"))
+
+    guess = 0
+
+    while(guess < max_guess):
+
+        guess = guess + 1
+
+        if(user == n):
+        
+         print(f"Great! you guess number in {guess} attempts.")
+
+         break
+
+        elif(user > n):
+
+            user = int(input("Guess a lower number please!"))
+
+        else:
+
+            user = int(input("Guess a higher number please!"))
+
+    if(guess == max_guess):
+                
+                print("You used your full attempts without guessing correct number")
+
+    choice = input("Do you want to play the game again?(yes/no)")
+
+print(" Thanks for playing the game. ")
